6e8784c684df889ee8afd3ee800d471c1695ad38
game.crash <73080127+gamecrash534@users.noreply.github.com>
2025-10-03 14:15:16 +0200

    add compression util and implement compression types used by minecraft

---
+++ src/main/java/xyz/gamecrash/AnvilAccess/util/compression/CompressionType.java
@@ -0,0 +1,23 @@
+package xyz.gamecrash.AnvilAccess.util.compression;
+
+import lombok.Getter;
+
+public enum CompressionType {
+    GZIP(1),
+    ZLIB(2),
+    UNCOMPRESSED(3),
+    LZ4(4),
+    ANY(127);
+
+    @Getter private final int id;
+
+    CompressionType(int id) { this.id = id; }
+
+    public static CompressionType fromId(int id) {
+        for (CompressionType type : values()) {
+            if (id == 127) throw new UnsupportedOperationException("Compression type " + id + " is not supported");
+            if (type.id == id) return type;
+        }
+        throw new IllegalArgumentException("Unknown compression type: " + id);
+    }
+}
