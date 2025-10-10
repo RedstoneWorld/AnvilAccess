c282368f052718f6f6393340d314349b0ac2473a
gamecrash534 <73080127+gamecrash534@users.noreply.github.com>
2025-10-10 14:04:52 +0200

    add missing javadoc comment for ChunkData

--- src/main/java/xyz/gamecrash/AnvilAccess/model/ChunkData.java
+++ src/main/java/xyz/gamecrash/AnvilAccess/model/ChunkData.java
@@ -1,3 +1,9 @@
 package xyz.gamecrash.AnvilAccess.model;
 
+/**
+ * Represents the chunk data of an MCA file
+ * @param data the raw chunk data
+ * @param length the chunk data length
+ * @param compressionType the type of compression used (view CompressionType for types)
+ */
 public record ChunkData(int length, byte compressionType, byte[] data) { }
